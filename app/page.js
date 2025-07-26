4500d4123b6c55d650fa5dfeacfc01a14044eee5
udaythakare <udaythakare592004@gmail.com>
2025-07-26 12:21:44 +0530

    wowo

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      hello world heheheheheheh
+      hello world i have deployed this app to my own vercel that is coolify
     </div>
   );
 }
